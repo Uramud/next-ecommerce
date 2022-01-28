2c87f4979e47f7f7996abf0ebfd21276fffd7de2
Uramud <50415534+Uramud@users.noreply.github.com>
2022-01-28 20:35:29 +0545

    Update - 4, adding static product details
    Download 5 images  (shirt, pant, tie, shoe, watch)
    Create 5 static data : utils/data.js
    importing those data.js in index .js
    use of:
           (Grid, Card, CardActionArea, CardMedia, CardContent)

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,43 @@
 // import Image from 'next/image';
 // import styles from '../styles/Home.module.css';
 
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 import Layout from '../components/layout';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+
+        {/* parent grid */}
+        <Grid container spacing={3}>
+          {/* child grid */}
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                </CardActionArea>
+                <CardContent>
+                  <Typography>{product.name}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
---
+++ utils/data.js
@@ -0,0 +1,62 @@
+//object with in object
+const data = {
+  products: [
+    {
+      name: 'CheckBox Shirt',
+      Category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: '$20',
+      brand: 'Louis Vuitton',
+      rating: 4.2,
+      numReviews: 10,
+      countInStock: 6,
+      description: 'Unique shirt',
+    },
+    {
+      name: 'Tie',
+      Category: 'Ties',
+      image: '/images/tie1.jpg',
+      price: '$5',
+      brand: 'Armani',
+      rating: 4.2,
+      numReviews: 19,
+      countInStock: 4,
+      description: 'Unique tie',
+    },
+    {
+      name: 'Jeans Pants',
+      Category: 'Pants',
+      image: '/images/pant1.jpg',
+      price: '$15',
+      brand: 'Gucci',
+      rating: 3.5,
+      numReviews: 4,
+      countInStock: 8,
+      description: 'Unique pant',
+    },
+    {
+      name: 'Boots',
+      Category: 'Shoes',
+      image: '/images/shoe1.jpg',
+      price: '$30',
+      brand: 'Timberland',
+      rating: 4.5,
+      numReviews: 15,
+      countInStock: 2,
+      description: 'Unique shirt',
+    },
+    {
+      name: 'Watch',
+      Category: 'watches',
+      image: '/images/watch1.jpg',
+      price: '$16000',
+      brand: 'Rolex',
+      rating: 4.9,
+      numReviews: 10,
+      countInStock: 2,
+      description: 'Unique watch',
+    },
+  ],
+};
+
+export default data;
